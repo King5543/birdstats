5bc7158f9b3ed76e8b4a6583ef2486fa4fef18af
Austin King <aking8@umbc.edu>
2020-02-25 15:24:54 -0500

    squirtle stuff

---
+++ squirtle_facts.py
@@ -0,0 +1,11 @@
+import json
+
+if __name__ == '__main__':
+    with open("squirtle.json", 'r') as f:
+        squirtle_facts = json.load(f)
+        print(json.dump(squirtle_facts, indent=2))
+    for ability in squirtle_facts['abilities']:
+        print(ability['ability']['name'])
+    for move_dict in squirtle_facts['moves']:
+        print(move_dict['move']['name'], 'is an ability!')
+    print('Loaded!')
